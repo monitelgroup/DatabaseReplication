443dd250c7c976459f6b7232fbeee8e59ab87719
Anton Lisitsyn <ra6fho@yandex.ru>
2011-06-06 02:14:51 +0400

    Написал класс в котором можно хранить информацию о таблице

---
+++ replication/replication/SqlTableStruct.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace replication
+{
+    /// <summary>
+    /// Класс обеспечивающий хранение данных о структуре таблицы
+    /// </summary>
+    class SqlTableStruct
+    {
+        /// <summary>
+        /// Количество столбцов в таблице
+        /// </summary>
+        private int _columnCount;
+        
+        /// <summary>
+        /// Количество столбцов в таблице
+        /// </summary>
+        public int ColumnCount
+        {
+            get { return _columnCount; }
+            set { _columnCount = value; }
+        }
+
+        /// <summary>
+        /// Имена столбцов в таблице
+        /// </summary>
+        private List<String> _columnNames;
+
+        /// <summary>
+        /// Имена столбцов в таблице
+        /// </summary>
+        public List<String> ColumnNames
+        {
+            get { return _columnNames; }
+            set { _columnNames = value; }
+        }
+
+        /// <summary>
+        /// Типы данных столбцов в таблице (sql-типы)
+        /// </summary>
+        private List<String> _columnSqlTypes;
+
+        /// <summary>
+        /// Типы данных столбцов в таблице (sql-типы)
+        /// </summary>
+        public List<String> ColumnSqlTypes
+        {
+            get { return _columnSqlTypes; }
+            set { _columnSqlTypes = value; }
+        }
+        
+        public SqlTableStruct()
+        {
+            this._columnCount = 0;
+            this._columnNames = new List<string>();
+            this._columnSqlTypes = new List<string>();
+        }
+    }
+}
