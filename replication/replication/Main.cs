be24b1c4f876e091b35b8d14b173ba4c4232e835
Anton Lisitsyn <ra6fho@yandex.ru>
2011-06-06 02:11:49 +0400

    Запуск репликации одно таблицы в консольном режиме

--- replication/replication/Main.cs
+++ replication/replication/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace replication
 {
@@ -6,7 +7,10 @@ namespace replication
 	{
 		public static void Main (string[] args)
 		{
-
-        	}
+			Configurator config = new Configurator();
+            ReplicationProcess testReplica = new ReplicationProcess(config);
+			EventTimer tmr = new EventTimer(config.MainTimerValue, testReplica.OnTimedEvent);
+			tmr.Start();
+        }
 	}
 }
