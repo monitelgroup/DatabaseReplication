383ff7602ef962670d6e7099e7c721300e3e406c
Anton Lisitsyn <ra6fho@yandex.ru>
2011-06-06 02:10:59 +0400

    Добавил комментарии

--- replication/replication/EMailSender.cs
+++ replication/replication/EMailSender.cs
@@ -4,16 +4,56 @@ using System.Net.Mail;
 
 namespace replication
 {
+    /// <summary>
+    /// Класс отправки Email сообщений
+    /// </summary>
 	public class EMailSender
 	{
+        /// <summary>
+        /// Адресс от которого будет отправлено сообщение
+        /// </summary>
 		string _emailAdress;
+
+        /// <summary>
+        /// Адрес smtp сервера
+        /// </summary>
 		string _smtpHost;
+
+        /// <summary>
+        /// Имя пользователя на сервере smtp
+        /// </summary>
 		string _userName;
-		string _userPassword;
-		int _smtpPort;
+		
+        /// <summary>
+        /// Пароль пользователя на сервере smtp
+        /// </summary>
+        string _userPassword;
+		
+        /// <summary>
+        /// Порт сервера smtp
+        /// </summary>
+        int _smtpPort;
 		
 		SmtpClient _smtp;
 		
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="emailAddress">
+        /// Адресс от которого будет отправлено сообщение
+        /// </param>
+        /// <param name="smtpHost">
+        /// Адрес smtp сервера
+        /// </param>
+        /// <param name="userName">
+        /// Имя пользователя на сервере smtp
+        /// </param>
+        /// <param name="userPassword">
+        /// Пароль пользователя на сервере smtp
+        /// </param>
+        /// <param name="smtpPort">
+        /// Порт сервера smtp
+        /// </param>
 		public EMailSender(string emailAddress, string smtpHost, string userName, string userPassword, int smtpPort){
 			this._emailAdress = emailAddress;
 			this._smtpHost = smtpHost;
@@ -24,6 +64,15 @@ namespace replication
 			this._smtp.Credentials = new NetworkCredential(this._userName, this._userPassword);
 		}
 		
+        /// <summary>
+        /// Отправляет сообщение
+        /// </summary>
+        /// <param name="message">
+        /// Текст сообщения
+        /// </param>
+        /// <param name="adminEMail">
+        /// Адресс куда будет отправлено сообщение
+        /// </param>
 		public void SendMessage(string message, string adminEMail) {
 			MailMessage msg = new MailMessage();
 			msg.From = new MailAddress(this._emailAdress);
